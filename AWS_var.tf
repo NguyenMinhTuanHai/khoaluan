c49de8c8b7f0a73c36ec71ac83a2e60945ca21e3
Hai <17520427@gm.uit.edu.vn>
2021-03-21 19:33:48 +0700

    modified:   AWS_var.tf

--- AWS_var.tf
+++ AWS_var.tf
@@ -2,7 +2,7 @@ variable "ec2-key" {
   default = "ssh_key"
 }
 
-//mã ami lấy tại https://wiki.centos.org/Cloud/AWS#head-78d1e3a4e6ba5c5a3847750d88266916ffe69648
+//mã ami lấy tại https://wiki.centos.org/Cloud/AWS#head-78d1e3a4e6ba5c5a3847750d88266916ffe6
 variable "ami" {
   default = "ami-0adfdaea54d40922b"
 }
