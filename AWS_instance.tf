69894f74bb5b7786b160704c8a4a6dbfd61b38be
Hai <17520427@gm.uit.edu.vn>
2021-04-10 01:10:50 +0700

    tự động hóa tạo file inventory

--- AWS_instance.tf
+++ AWS_instance.tf
@@ -4,16 +4,27 @@ resource "aws_instance" "web-server" {
   key_name               = var.ec2-key
   vpc_security_group_ids = [aws_security_group.allow_all_ssh_http_https.id]
 
-  /*provisioner "local-exec" {
-    command = "ssh-keyscan -H ${aws_instance.web-server.public_ip} >> ~/.ssh/known_hosts"
+
+
+  #tạo file inventory với thông tin máy ảo sẵn sàng
+  provisioner "local-exec" {
+    command = "echo [ansible_client] > inventory"
   }
 
   provisioner "local-exec" {
+    command = "echo ${aws_instance.web-server.public_ip} ansible_ssh_user=centos ansible_ssh_private_key_file=ssh_key.pem >> inventory"
+  }
+  
+
+
+  /*provisioner "local-exec" {
     command = "scp -i ssh_key.pem -r wordpress-source centos@${aws_instance.web-server.public_ip}:~"
   }*/
 
+
+
   # Tạo thư mục lưu file trong wordpress-source
-  provisioner "remote-exec" {
+  /*provisioner "remote-exec" {
     inline = [
       "mkdir ~/wordpress-source-remote/",
     ]
@@ -24,10 +35,10 @@ resource "aws_instance" "web-server" {
       private_key = file(var.ssh_key)
       host        = aws_instance.web-server.public_ip
     }
-  }
+  }*/
 
   # Copy tất cả file trong thư mục wordpress-source đến /home/centos/wordpress-source-remote/
-  provisioner "file" {
+  /*provisioner "file" {
     source      = "wordpress-source/"
     destination = "~/wordpress-source-remote/"    /* ~/ = /home/centos */
     
@@ -37,7 +48,7 @@ resource "aws_instance" "web-server" {
       private_key = file(var.ssh_key)
       host        = aws_instance.web-server.public_ip
     }
-  }
+  }*/
 
   tags = {
     Name = "terraform-web-server"
