54d04a8ce3c5e024785a5e90f6e93f88d7469709
Hai <17520427@gm.uit.edu.vn>
2021-04-10 21:47:52 +0700

    update sửa lỗi syntax

--- AWS_instance.tf
+++ AWS_instance.tf
@@ -2,11 +2,10 @@ resource "aws_instance" "web-server" {
   ami                    = var.ami
   instance_type          = "t2.micro"
   key_name               = var.ec2-key
-  vpc_security_group_ids = [aws_security_group.allow_all_ssh_http_https.id]
+  vpc_security_group_ids = [aws_security_group.allow_all_ssh_http_https.id] 
+  
 
-
-
-  #tạo file inventory với thông tin máy ảo sẵn sàng
+  // tạo file inventory với thông tin máy ảo sẵn sàng
   provisioner "local-exec" {
     command = "echo [ansible_client] > inventory"
   }
@@ -15,15 +14,9 @@ resource "aws_instance" "web-server" {
     command = "echo ${aws_instance.web-server.public_ip} ansible_ssh_user=centos ansible_ssh_private_key_file=ssh_key.pem >> inventory"
   }
   
+  
 
-
-  /*provisioner "local-exec" {
-    command = "scp -i ssh_key.pem -r wordpress-source centos@${aws_instance.web-server.public_ip}:~"
-  }*/
-
-
-
-  # Tạo thư mục lưu file trong wordpress-source
+  // Tạo thư mục lưu file trong wordpress-source
   /*provisioner "remote-exec" {
     inline = [
       "mkdir ~/wordpress-source-remote/",
@@ -37,10 +30,10 @@ resource "aws_instance" "web-server" {
     }
   }*/
 
-  # Copy tất cả file trong thư mục wordpress-source đến /home/centos/wordpress-source-remote/
+  // Copy tất cả file trong thư mục wordpress-source đến /home/centos/wordpress-source-remote/
   /*provisioner "file" {
     source      = "wordpress-source/"
-    destination = "~/wordpress-source-remote/"    /* ~/ = /home/centos */
+    destination = "~/wordpress-source-remote/"    /* ~/ = /home/centos 
     
     connection {
       type        = "ssh"
@@ -53,4 +46,5 @@ resource "aws_instance" "web-server" {
   tags = {
     Name = "terraform-web-server"
   }
+  
 }
