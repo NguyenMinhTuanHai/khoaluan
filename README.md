75a5dc98a23a5bbda2e68616dbbc6cbfec9e57c3
Hai <17520427@gm.uit.edu.vn>
2021-04-18 01:05:13 +0700

    update code install nginx + mariadb (start service auto)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # khoaluan
 # Lưu file ssh_key.pem tại thư mục này
+# 1 số câu lệnh cần thiết
+# systemctl start mariadb
+# mysql_secure_installation
+# sudo passwd root
+# systemctl status nginx
--- scripts/php_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo yum -y install yum-utils epel-release
-sudo rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-sudo yum-config-manager --enable remi-php73
-sudo yum -y install php-fpm php-ldap php-zip php-embedded php-cli php-mysql php-common php-gd php-xml php-mbstring php-mcrypt php-pdo php-soap php-json php-simplexml php-process php-curl php-bcmath php-snmp php-pspell php-gmp php-intl php-imap perl-LWP-Protocol-https php-pear-Net-SMTP php-enchant php-pear php-devel php-zlib php-xmlrpc php-tidy php-mysqlnd php-opcache php-cli php-pecl-zip unzip gcc
